afb93c38c585c4990162d90543b9fd03b2ac3e15
daskro410 <72729971+daskro410@users.noreply.github.com>
2021-09-27 13:43:58 +0200

    Update Main.java

--- Server/src/com/company/Main.java
+++ Server/src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
         int port = 6969;
+        DataInputStream in;
+        DataOutputStream out;
+        ServerSocket server;
+        Socket socket;
     }
 }
