3cb67cd07d0a28d4ba501e117beef85015f59c8a
daskro410 <72729971+daskro410@users.noreply.github.com>
2021-09-28 13:13:32 +0200

    Update Main.java

--- Server/src/com/company/Main.java
+++ Server/src/com/company/Main.java
@@ -6,9 +6,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+    int port = 6969;
 
     public static void main(String[] args) {
-        int port = 6969;
         DataInputStream in;
         DataOutputStream out;
         ServerSocket server;
