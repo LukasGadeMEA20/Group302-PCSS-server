0cd5ef5396f4da41100ed4cb70353f4aead84139
Lukas Gade Ravnsborg <lravns20@student.aau.dk>
2021-10-08 10:39:23 +0200

    Server

--- Server/src/com/company/Main.java
+++ Server/src/com/company/Main.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Main {
@@ -16,7 +17,8 @@ public class Main {
                 ServerSocket serverSocket = new ServerSocket(12345);
                 System.out.println("Server started at " + new Date() + '\n');
 
-                ServerUser[] listOfUsers = new ServerUser[]{new ServerUser("blank")};
+                ArrayList<ServerUser> listOfUsers= new ArrayList<ServerUser>();
+                //ServerUser[] listOfUsers = new ServerUser[]{new ServerUser("blank")};
                 int clientNo = 0;
                 while(true) {
                     Socket connectToClient = serverSocket.accept();
@@ -28,14 +30,16 @@ public class Main {
                     System.out.println("Client " + clientNo + "'s host name is " + inetAddress.getHostName() + '\n');
                     System.out.println("Client " + clientNo + "'s host address is " + inetAddress.getHostAddress() + '\n');
                     new Thread(
-                            new WorkerRunnable(connectToClient, "Multithreadded server", inetAddress.getHostName())
+                            new WorkerRunnable(connectToClient, "Multithreadded server", inetAddress.getHostAddress())
                     ).start();
-
-                    DataInputStream fromFile = new DataInputStream(new FileInputStream(inetAddress.getHostName()+".txt"));
-                    listOfUsers[thisUserNumber] = new ServerUser(fromFile.readUTF());
+                    Thread.sleep(5000);
+                    DataInputStream fromFile = new DataInputStream(new FileInputStream(inetAddress.getHostAddress()+".txt"));
+                    listOfUsers.add(new ServerUser(fromFile.readUTF()));
                     System.out.println(thisUserNumber);
                 }
             } catch (IOException e){
+                System.err.println(e);
+            } catch(InterruptedException e){
                 System.err.println(e);
             }
         }).start();
