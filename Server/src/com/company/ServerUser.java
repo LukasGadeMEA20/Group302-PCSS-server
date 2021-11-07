671bdd9b456d989d4f0f3c67dab0c529e6ae41bb
Lukas Gade Ravnsborg <lravns20@student.aau.dk>
2021-11-07 17:13:22 +0100

    Commentation of server user

--- Server/src/com/company/ServerUser.java
+++ Server/src/com/company/ServerUser.java
@@ -1,31 +1,32 @@
 package com.company;
 
 public class ServerUser {
+    // Information about the user
     String userName = "";
     int points= 0;
     int userID = 0;
     String ipName = "";
 
+    // Constructor for creating a user with a username
     ServerUser(String _userName, int _id, String _ipName){
         userName = _userName;
         userID = _id;
         ipName = _ipName;
     }
 
+    // Constructor for creating a user without a username
     ServerUser(int _id, String _ipName){
         userID = _id;
         ipName = _ipName;
     }
+    
+    // - Getters and setters - //
     public String getUserName(){
         return userName;
     }
 
     public void setPoints(int points) { //sets the points of a user
         this.points = points;
-    }
-
-    public void delegatePoint(){
-        points++;
     }
 
     public int getPoints() { //gets the points of a user
@@ -42,5 +43,10 @@ public class ServerUser {
 
     public String getIpName(){
         return ipName;
+    }
+    
+    // Delegating a singular point to the user.
+    public void delegatePoint(){
+        points++;
     }
 }
