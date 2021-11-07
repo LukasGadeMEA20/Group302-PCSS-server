ebff4e9d983615417f46632619e76fcf6133adf7
Lukas Gade Ravnsborg <lravns20@student.aau.dk>
2021-11-07 17:14:55 +0100

    Commentated user answer

--- Server/src/com/company/UserAnswer.java
+++ Server/src/com/company/UserAnswer.java
@@ -1,10 +1,14 @@
 package com.company;
 
+/*
+    A very simple class which store which user wrote in the answer,
+    what the answer is and a simple boolean to make sure the program knows if it is ready.
+*/
 public class UserAnswer {
     ServerUser user;
     String userAnswer;
     boolean ready;
-
+    
     UserAnswer(ServerUser _user, String _userAnswer, boolean _ready){
         user = _user;
         userAnswer = _userAnswer;
